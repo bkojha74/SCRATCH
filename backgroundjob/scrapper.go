536a6712d05458b55c5cea9c567d8f367f825549
bkojha <bkojha74@gmail.com>
2024-02-01 21:01:56 +0530

    Saves RSS Feeds to PostgreSQL

--- backgroundjob/scrapper.go
+++ backgroundjob/scrapper.go
@@ -2,13 +2,16 @@ package backgroundjob
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/bkojha74/rssagg/internal/database"
 	"github.com/bkojha74/rssagg/models/rss"
+	"github.com/google/uuid"
 )
 
 func StartScrapping(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
@@ -45,7 +48,34 @@ func scrapFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found Post: ", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description = sql.NullString{String: item.Description, Valid: true}
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("couldn't parse date %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			fmt.Println("Failed to create post: ", err)
+		}
+
 	}
 	log.Printf("Feed %s collected. %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
--- models/models.go
+++ models/models.go
@@ -78,3 +78,39 @@ func DatabaseFeedFollowersMap(dbFeedFollows []database.FeedFollow) []FeedFollow 
 	}
 	return feedFollows
 }
+
+type Post struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description *string   `json:"description"`
+	PublishedAt time.Time `json:"published_at"`
+	Url         string    `json:"url"`
+	FeedID      uuid.UUID `json:"feed_id"`
+}
+
+func DatabasePostMap(dbPost database.Post) Post {
+	var description *string
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+	return Post{
+		ID:          dbPost.ID,
+		CreatedAt:   dbPost.CreatedAt,
+		UpdatedAt:   dbPost.UpdatedAt,
+		Title:       dbPost.Title,
+		Description: description,
+		PublishedAt: dbPost.PublishedAt,
+		Url:         dbPost.Url,
+		FeedID:      dbPost.FeedID,
+	}
+}
+
+func DatabasePostsMap(dbPost []database.Post) []Post {
+	var posts []Post
+	for _, post := range dbPost {
+		posts = append(posts, DatabasePostMap(post))
+	}
+	return posts
+}
